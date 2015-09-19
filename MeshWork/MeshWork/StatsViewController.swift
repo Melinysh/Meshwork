89ced881ba703a016ee3d5e8a98693a102c8e025
Jonathan Galperin <jonathan@galperin.com>
2015-09-19 10:12:27 -0400

    New view controller,

--- MeshWork/MeshWork/StatsViewController.swift
+++ MeshWork/MeshWork/StatsViewController.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+import UIKit
+
+class StatsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.layoutViews()
+    }
+    
+    func layoutViews() {
+        self.view.backgroundColor = UIColor.whiteColor()
+        let label = UILabel(frame: CGRectMake(100, 100, 100, 20))
+        label.text = "How are you"
+        self.view.addSubview(label)
+    }
+}
