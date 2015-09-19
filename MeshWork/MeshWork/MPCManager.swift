9ff7b694d69395c4f9891c55f8cd34674d929a65
Stephen Melinyshyn <smelinysh@icloud.com>
2015-09-19 00:38:27 -0400

    Completed foundation of MPCManager

--- MeshWork/MeshWork/MPCManager.swift
+++ MeshWork/MeshWork/MPCManager.swift
@@ -10,52 +10,46 @@ import UIKit
 import MultipeerConnectivity
 
 protocol MPCManagerDelegate {
-  func foundPeer()
-  
-  func lostPeer()
-  
-  func invitationWasReceived(fromPeer: String)
-  
-  func connectedWithPeer(peerID: MCPeerID)
-  
-  var browser : UIWebView! { get set }
-  var lastRequest : NSURLRequest! { get set }
+	func foundPeer(peer : MCPeerID)
+	func lostPeer(peer : MCPeerID)
+	func invitationWasReceived(fromPeer: String)
+	func receievedContactFromPeer(peer : MCPeerID, contact : ContactObject)
 }
 
-class MPCManager : NSObject, MCSessionDelegate, MCNearbyServiceAdvertiserDelegate, MCNearbyServiceBrowserDelegate,  {
+class MPCManager : NSObject, MCSessionDelegate, MCNearbyServiceAdvertiserDelegate, MCNearbyServiceBrowserDelegate  {
   
   var session : MCSession!
-  let peer = MCPeerID(displayName: UIDevice.currentDevice().name)
+  let selfPeer = MCPeerID(displayName: UIDevice.currentDevice().name)
+  var selfContact : ContactObject!
   var browser : MCNearbyServiceBrowser!
   var advertiser: MCNearbyServiceAdvertiser!
   
-  var foundPeer : (peer: MCPeerID?, info: [String : String]?)
+  var foundPeers : [String : MCPeerID] = [String : MCPeerID]()
   var invitationHandler : ((Bool, MCSession) -> Void)!
   
   var delegate : MPCManagerDelegate!
   
-  init(delegate : MPCManagerDelegate) {
+  init(delegate : MPCManagerDelegate, selfContact : ContactObject) {
     super.init()
     
     self.delegate = delegate
-    session = MCSession(peer: peer)
+    self.selfContact = selfContact
+    session = MCSession(peer: selfPeer)
     session.delegate = self
-    browser = MCNearbyServiceBrowser(peer: peer, serviceType: "meshwurk")
+    browser = MCNearbyServiceBrowser(peer: selfPeer, serviceType: "meshwurk")
     browser.delegate = self
-    advertiser = MCNearbyServiceAdvertiser(peer: peer, discoveryInfo: ["Test" : "This is a test that should be replaced"], serviceType: "meshwurk")
+    advertiser = MCNearbyServiceAdvertiser(peer: selfPeer, discoveryInfo: nil, serviceType: "meshwurk")
     advertiser.delegate = self
     
   }
   
   @objc func browser(browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
-    foundPeer = (peerID, info)
-    delegate.foundPeer()
-    
+    foundPeers[peerID.displayName] = peerID
   }
   
   @objc func browser(browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
-    foundPeer = (nil, nil)
-    delegate.lostPeer()
+    foundPeers.removeValueForKey(peerID.displayName)
+    delegate.lostPeer(peerID)
   }
   
   @objc func browser(browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: NSError) {
@@ -87,20 +81,29 @@ class MPCManager : NSObject, MCSessionDelegate, MCNearbyServiceAdvertiserDelegat
   }
   
   @objc func session(session: MCSession, didReceiveData data: NSData, fromPeer peerID: MCPeerID) {
-       
+    guard let contact = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? ContactObject else {
+		print("Looks like the recieved object is not a contact :(.")
+		return
+    }
+	delegate.receievedContactFromPeer(peerID, contact: contact)
+	
   }
   
   @objc func session(session: MCSession, peer peerID: MCPeerID, didChangeState state: MCSessionState) {
     switch state {
-    case MCSessionState.Connected :
-      print("Connected to session \(session)")
-      delegate.connectedWithPeer(peerID)
-      
-    case MCSessionState.Connecting :
-      print("Connecting to session \(session)")
-      
-    default:
-      print("Did not connect to session \(session)")
+        case MCSessionState.Connected :
+        print("Connected to session \(session)")
+        do {
+            try session.sendData(NSKeyedArchiver.archivedDataWithRootObject(selfContact), toPeers: [peerID], withMode: .Reliable)
+        } catch {
+            print("Error sending over contact \(error)")
+        }
+
+        case MCSessionState.Connecting :
+        print("Connecting to session \(session)")
+
+        default:
+        print("Did not connect to session \(session)")
     }
   }
 
